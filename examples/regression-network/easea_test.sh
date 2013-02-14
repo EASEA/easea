bb0eef9e976ef264def8c95203cba373118135e3
wcancino <wcancino@gmail.com>
2013-02-14 15:33:03 +0100

    Modify JDL job files

--- examples/regression-network/easea_test.sh
+++ examples/regression-network/easea_test.sh
@@ -2,7 +2,7 @@
 export LCG_GFAL_INFOSYS=topbdii.grif.fr:2170
 export LCG_CATALOG_TYPE=lfc
 export LCG_GFAL_VO=vo.complex-systems.eu
-
+cat /etc/*-release
 chmod +x $PWD/regression
-$PWD/regression --nbGen=20
+$PWD/regression --nbGen=50
 
