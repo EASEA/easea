74e43dd9357bc45465a81fb8d89a4e229d202c46
wcancino <wcancino@gmail.com>
2013-03-21 00:21:55 +0100

    Improve network communication
    Update templates for the grid

--- libeasea/include/CComWorker.h
+++ libeasea/include/CComWorker.h
@@ -12,6 +12,7 @@ class CommWorker
     bool active;
     unsigned int nfails;
     bool internal_ip;
+    unsigned long int mynetmask;
   public:
     
     CommWorker(std::string wname, std::string wip, short unsigned int wport);
@@ -19,6 +20,8 @@ class CommWorker
     std::string get_name() const;
     std::string get_ip() const;
     short unsigned int get_port() const;
+    unsigned long int get_netmask() const;
+    void set_netmask(unsigned long int);
     bool isactive() const;
     void desactivate();
     void activate();
