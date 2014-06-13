630c65566aa0d5de29b45087bc7430a3f341cda9
pallamidessi <joseph.pallamidessi@gmail.com>
2014-06-13 13:51:06 +0200

    Update serialization of sub-structure

--- compiler/EaseaSym.cpp
+++ compiler/EaseaSym.cpp
@@ -264,7 +264,12 @@ void CSymbol::print(FILE *fp)
 				{
 					fprintf(fpOutputFile,"\tfor(int EASEA_Ndx=0; EASEA_Ndx<%d; EASEA_Ndx++)\n",
 							pSym->Object->nSize/pSym->Object->pType->nSize);
-					fprintf(fpOutputFile,"\t\tEASEA_Line << this->%s[EASEA_Ndx] <<\" \";\n", pSym->Object->sName);
+					fprintf(fpOutputFile,"\t\tEASEA_Line << this->%s[EASEA_Ndx].serializer() <<\" \";\n", pSym->Object->sName);
+				}
+				// it's a simple struct/class
+				else if(pSym->Object->ObjectType==oObject)
+				{
+					fprintf(fpOutputFile,"\t\tEASEA_Line << this->%s.serializer() <<\" \";\n", pSym->Object->sName);
 				}
 				else
 				{
@@ -306,6 +311,7 @@ void CSymbol::print(FILE *fp)
 		{
 			if(pSym->Object->pType->ObjectType==oUserClass)
 			{
+				/* it's an array of pointer */
 				if (pSym->Object->ObjectType==oArrayPointer)
 				{
 					fprintf(fp,"\tfor(int EASEA_Ndx=0; EASEA_Ndx<%d; EASEA_Ndx++){\n",
@@ -321,6 +327,19 @@ void CSymbol::print(FILE *fp)
 					fprintf(fpOutputFile,"\t\t}");
 					fprintf(fpOutputFile,"\t}");
 				}
+				/* it's a classical array*/
+				else if(pSym->Object->ObjectType==oArray)
+				{
+					fprintf(fpOutputFile,"\tfor(int EASEA_Ndx=0; EASEA_Ndx<%d; EASEA_Ndx++)\n",
+							pSym->Object->nSize/pSym->Object->pType->nSize);
+					fprintf(fpOutputFile,"\t\tthis->%s[EASEA_Ndx].deserializer(&EASEA_Line) ;\n", pSym->Object->sName);
+				}
+				/* it's a simple struct/class */
+				else if(pSym->Object->ObjectType==oObject)
+				{
+					fprintf(fpOutputFile,"\t\tthis->%s.deserializer(EASEA_Line);\n", pSym->Object->sName);
+				}
+				/*it's a pointer*/
 				else
 				{
 					fprintf(fpOutputFile,"\t(*EASEA_Line) >> line;\n");
@@ -577,6 +596,7 @@ void CSymbol::serializeIndividual(FILE *fp, char* sCompleteName)
 						pSym->Object->nSize/pSym->Object->pType->nSize);
 				fprintf(fpOutputFile,"\t\tEASEA_Line << this->%s[EASEA_Ndx].serializer() <<\" \";\n", pSym->Object->sName);
 			}
+			// it's a simple struct/class 
 			else if(pSym->Object->ObjectType==oObject)
 			{
 				fprintf(fpOutputFile,"\tEASEA_Line << \"\\a \";\n");
