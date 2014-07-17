b4266a93e0f4e262509280cb441a88240e8dcf69
pallamidessi <joseph.pallamidessi@gmail.com>
2014-07-17 13:24:27 +0200

    Add doxygen documentation

--- libeasea/include/Parameters.h
+++ libeasea/include/Parameters.h
@@ -4,6 +4,24 @@
  *  Created on: 23 juin 2009
  *      Author: maitre
  */
+/**
+ * @file Parameters.h
+ * @author SONIC BFO, Ogier Maitre 
+ * @version 1.0
+ *
+ * @section LICENSE
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation; either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Affero General Public License for more details at
+ * http://www.gnu.org/licenses/
+**/  
 
 #ifndef PARAMETERS_H_
 #define PARAMETERS_H_
@@ -13,19 +31,73 @@ class CGenerationalCriterion;
 class CControlCStopingCriterion;
 class CSelectionOperator;
 
+/**
+*  \class     Parameters 
+*  \brief     A class for passing various parametr to the main EA loop
+*  \details   The contents of this class are set by 1) tpl files 
+*                                                   2) *.prm at launch and 
+*                                                   3) command line arguments.
+*  
+**/
 class Parameters {
+  
   public:
+
+    /*TODO: Verify if this distinction is still needed on g++-mingw */
+    /*Constructors/Destructors----------------------------------------------------*/
+    #ifdef WIN32
+      Parameters();
+      ~Parameters();
+    #else
+      virtual ~Parameters(){;}
+    #endif
+    
+    /*Methods---------------------------------------------------------------------*/
+    /**
+    * \brief    Initialization from arguments
+    * \details  By using the standard C way. Defined in tpl files.
+    *
+    *  @param   argc  Number of arguments
+    *  @param   argv  Array of argument as char*
+    **/
+    virtual void setDefaultParameters(int argc, char** argv) = 0;
+    
+
+    /**
+    * \brief    Instanciate a new CEvolutionaryAlgorithm using current variables
+    * \details  Defined in tpl files.
+    *
+    * @return   var   The new EA 
+    **/
+    virtual CEvolutionaryAlgorithm* newEvolutionaryAlgorithm() = 0;
+
+
+    /**
+    * \brief    Set reduction size
+    * \details  Do correctness checking 
+    *
+    *  @param   popReducSize A percentage, represented by a float between 0 and 1
+    *  @param   popSize      The size to be reduced
+    * @return   var          The resulting size
+    **/
+    int setReductionSizes(int popSize, float popReducSize);
+  
+  public:
+    /*Datas-----------------------------------------------------------------------*/
+    //Reduction and selection operator
     CSelectionOperator* selectionOperator;
     CSelectionOperator* replacementOperator;
     CSelectionOperator* parentReductionOperator;
     CSelectionOperator* offspringReductionOperator;
-
+    
+    //Stopping criterion
     CGenerationalCriterion* generationalCriterion;
     CControlCStopingCriterion* controlCStopingCriterion;
     CTimeCriterion* timeCriterion;
 
     int nbGen;
 
+    //Reduction and selection pressures
     float selectionPressure;
     float replacementPressure;
     float parentReductionPressure;
@@ -36,7 +108,8 @@ class Parameters {
     float pMutation;
     float pMutationPerGene;
     CRandomGenerator* randomGenerator;
-
+    
+    //PRNG seed
     time_t seed;
 
     unsigned int parentPopulationSize;
@@ -77,20 +150,11 @@ class Parameters {
 
     char* outputFilename;
     char* plotOutputFilename;
-
+    
+    //GPU selection
     int fstGpu;
     int lstGpu;
 
-  public:
-#ifdef WIN32
-    Parameters();
-    ~Parameters();
-#else
-    virtual ~Parameters(){;}
-#endif
-    virtual void setDefaultParameters(int argc, char** argv) = 0;
-    virtual CEvolutionaryAlgorithm* newEvolutionaryAlgorithm() = 0;
-    int setReductionSizes(int popSize, float popReducSize);
 };
 
 #endif /* PARAMETERS_H_ */
