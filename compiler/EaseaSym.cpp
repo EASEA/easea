8b3244576aa0ac50bab4df529de88766230e3ed8
ivan.aksamentov <ivan.aksamentov@gmail.com>
2015-05-28 23:10:47 +0200

    fix typo in array size

--- compiler/EaseaSym.cpp
+++ compiler/EaseaSym.cpp
@@ -818,7 +818,7 @@ void CSymbol::deserializeIndividual(FILE *fp, char* sCompleteName){
 
 
 void CSymbol::printAllSymbols(FILE *fp, char *sCompleteName, EObjectType FatherType, CListItem<CSymbol *> *pSym){
-	char sNewCompleteName[000], s[20];
+	char sNewCompleteName[1000], s[20];
 	strcpy(sNewCompleteName, sCompleteName);
 	do {
 		if (pSym->Object->pType->ObjectType==oUserClass){
