d95b8b38c95f587fdc780200d4a3b1c2257cc11e
pallamidessi <joseph.pallamidessi@gmail.com>
2016-07-04 15:40:42 +0200

    Add nsga template

--- compiler/Easea.h
+++ compiler/Easea.h
@@ -44,19 +44,24 @@ class CSymbol;
 extern CSymbol *pCURRENT_CLASS, *pCURRENT_TYPE, *pGENOME, *pCLASSES[128];
 extern int nClasses_nb;
 
-extern   FILE *fpOutputFile, *fpTemplateFile, *fpGenomeFile, *fpExplodedGenomeFile;  
-extern char sRAW_PROJECT_NAME[], sPROJECT_NAME[], sEO_DIR[], sEZ_PATH[1000],  sTPL_DIR[1000], sEZ_FILE_NAME[];
+extern   FILE *fpOutputFile, *fpTemplateFile, *fpGenomeFile,
+         *fpExplodedGenomeFile;
+extern char sRAW_PROJECT_NAME[], sPROJECT_NAME[], sEO_DIR[], sEZ_PATH[1000],
+       sTPL_DIR[1000], sEZ_FILE_NAME[];
 extern char sLOWER_CASE_PROJECT_NAME[];
-extern char  sSELECTOR[], sSELECTOR_OPERATOR[], sRED_PAR[], sRED_PAR_OPERATOR[], sRED_FINAL[], sRED_FINAL_OPERATOR[], sRED_OFF[], sRED_OFF_OPERATOR[];
+extern char  sSELECTOR[], sSELECTOR_OPERATOR[], sRED_PAR[], sRED_PAR_OPERATOR[],
+       sRED_FINAL[], sRED_FINAL_OPERATOR[], sRED_OFF[], sRED_OFF_OPERATOR[];
 extern char sIP_FILE[];
 extern bool bELITISM, bVERBOSE;
 extern bool bBALDWINISM;
-extern bool bPRINT_STATS, bPLOT_STATS, bGENERATE_CSV_FILE, bGENERATE_GNUPLOT_SCRIPT, bGENERATE_R_SCRIPT;
+extern bool bPRINT_STATS, bPLOT_STATS, bGENERATE_CSV_FILE,
+       bGENERATE_GNUPLOT_SCRIPT, bGENERATE_R_SCRIPT;
 extern bool bSAVE_POPULATION, bSTART_FROM_FILE;
 extern bool bREMOTE_ISLAND_MODEL;
 extern bool bLINE_NUM_EZ_FILE;
 extern char* nGENOME_NAME;
-extern int nPOP_SIZE, nNB_GEN, nNB_OPT_IT, nOFF_SIZE, nPROBLEM_DIM, nTIME_LIMIT;
+extern int nPOP_SIZE, nNB_GEN, nNB_OBJECTIVE, nNB_OPT_IT, nOFF_SIZE,
+       nPROBLEM_DIM, nTIME_LIMIT;
 extern int nSERVER_PORT;
 extern int nWARNINGS, nERRORS;
 extern int TARGET, OPERATING_SYSTEM;
@@ -67,7 +72,8 @@ extern float fSELECT_PRM, fRED_PAR_PRM, fRED_FINAL_PRM, fRED_OFF_PRM;
 extern float fMIGRATION_PROBABILITY;
 extern int fCOUNT;
 
-extern unsigned iMAX_INIT_TREE_D,iMIN_INIT_TREE_D,iMAX_TREE_D,iNB_GPU,iPRG_BUF_SIZE,iMAX_TREE_DEPTH,iNO_FITNESS_CASES;
+extern unsigned iMAX_INIT_TREE_D,iMIN_INIT_TREE_D,iMAX_TREE_D,iNB_GPU,
+       iPRG_BUF_SIZE,iMAX_TREE_DEPTH,iNO_FITNESS_CASES;
 
 // Prototypes
 extern int mystricmp(const char *, const char *);
