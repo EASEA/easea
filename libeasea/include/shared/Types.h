941ca7c1f03c01dec040273d68c07408dcdc2b9e
root <leopard152015@gmail.com>
2020-08-18 14:39:32 +0200

    added header for std::random

--- libeasea/include/shared/Types.h
+++ libeasea/include/shared/Types.h
@@ -11,7 +11,7 @@
 |                                                                       |
  ***********************************************************************/
 #pragma once
-
+#include <random>
 namespace easea{
 namespace shared {
 
