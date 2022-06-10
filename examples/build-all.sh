da2a67353875bf7c72a5d88683a556b34676ebbf
zeFresk <8807862+zeFresk@users.noreply.github.com>
2022-06-10 20:32:37 +0200

    More logs if error when executing

--- examples/build-all.sh
+++ examples/build-all.sh
@@ -91,7 +91,7 @@ for edir in $all_examples; do
 
 	# run
 	printf "\tExecuting %s ..." "$EASEA_OUT"
-	OUT=$(timeout -k 2 1 "./$EASEA_OUT")
+	OUT=$((timeout -k 2 1 "./$EASEA_OUT") 2>&1)
 	ret=$?
 	if [[ "$ret" == "0" ]] || [[ "$ret" == "124" ]]; then # ok
 		printf "$Green ok!$Color_Off\n"
