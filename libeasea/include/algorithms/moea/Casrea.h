f7095cfcc9f2945b5733ac6e9c68d6cc233250c2
zeFresk <8807862+zeFresk@users.noreply.github.com>
2022-06-23 18:51:47 +0200

    Start of solving -asrea bug

--- libeasea/include/algorithms/moea/Casrea.h
+++ libeasea/include/algorithms/moea/Casrea.h
@@ -140,11 +140,18 @@ void Casrea<TIndividual, TRandom>::makeOneGeneration(void)
 	size_t szPopDiv2 = (offspring.size() - TBaseArchive::m_archive.size()) / 2;
 	size_t icounter = TBaseArchive::m_archive.size();
 
-	std::vector<TPtr> archPop;
+	std::copy(TBaseArchive::m_archive.begin() /*+ std::max(0, static_cast<int>(TBaseArchive::m_archive.size()) - static_cast<int>(TBase::m_population.size()))*/, TBaseArchive::m_archive.begin() + std::min(TBaseArchive::m_archive.size(), TBase::m_population.size()), TBase::m_population.begin());
+	if (TBaseArchive::m_archive.size() > TBase::m_population.size())
+		std::copy(TBaseArchive::m_archive.begin() + TBase::m_population.size(), TBaseArchive::m_archive.end() , std::back_inserter(TBase::m_population));
+
+	LOG_MSG(msgType::DEBUG, "pop size: %d\narchive size: %d\n", TBase::m_population.size(), TBaseArchive::m_archive.size());
+	// NOTE: old version below used totalSelection and was responsible for alignment errors and segfault
+	/*
 	for (size_t i = 0; i < TBaseArchive::m_archive.size(); ++i)
     		archPop.push_back(&TBaseArchive::m_archive[i]);
 	easea::operators::selection::totalSelection(archPop, TBase::m_population.begin(), TBase::m_population.end());
-	
+	*/
+
 	for( size_t i = icounter; i < szPopDiv2; ++i)
 	{
 		std::uniform_int_distribution<size_t> dist(0, TBaseArchive::m_archive.size() - 1);
