2e9e7061172456acdba6b3342e36270403e2a7ec
root <leopard152015@gmail.com>
2023-06-04 08:57:56 +0200

    all dtlz examples are set up for nsgaii

--- examples/dtlz/dtlz4/compile.sh
+++ examples/dtlz/dtlz4/compile.sh
@@ -2,7 +2,7 @@
 
 #Place here -nsgaiii / -nsgaii / -cdas
 echo "Compiling .ez to .cpp .hpp files..."
-$EZ_PATH/bin/easena -asrea  dtlz4.ez
+$EZ_PATH/bin/easena -nsgaii  dtlz4.ez
 echo "Compiling .cpp file..."
 make
 echo "Done"
--- examples/dtlz/dtlz6/compile.sh
+++ examples/dtlz/dtlz6/compile.sh
@@ -2,7 +2,7 @@
 
 #Place here -nsgaiii / -nsgaii / -cdas
 echo "Compiling .ez to .cpp .hpp files..."
-$EZ_PATH/bin/easena -fastemo  dtlz6.ez
+$EZ_PATH/bin/easena -nsgaii  dtlz6.ez
 echo "Compiling .cpp file..."
 make
 echo "Done"
