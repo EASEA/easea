4b991be9ed488170955bb0b547e1915941c5565f
zeFresk <8807862+zeFresk@users.noreply.github.com>
2023-07-08 16:38:21 +0200

    More fixes of UR

---
+++ libeasea/CProblem.cpp
@@ -0,0 +1,5 @@
+#include <problems/CProblem.h>
+
+// reduce compilation time and check for errors while compiling lib
+template class easea::problem::CProblem<double>;
+template class easea::problem::CProblem<float>;
