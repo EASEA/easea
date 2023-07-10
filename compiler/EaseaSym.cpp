67e7117f0734aee6aecd494f71e10a2c5dbdd2f1
zeFresk <8807862+zeFresk@users.noreply.github.com>
2023-07-10 13:39:24 +0200

    Fix compiler warning inversed

--- compiler/EaseaSym.cpp
+++ compiler/EaseaSym.cpp
@@ -861,7 +861,7 @@ void CSymbol::printAllSymbols(FILE* fp, char* sCompleteName, EObjectType FatherT
 
 CSymbol* CSymbolTable::insert(std::unique_ptr<CSymbol>&& symbol)
 {
-	if (hashmap.find(symbol->sName) != hashmap.end() && symbol->sName.contains("CUSTOM_PRECISION_TYPE")) {
+	if (hashmap.find(symbol->sName) != hashmap.end() && symbol->sName.compare("CUSTOM_PRECISION_TYPE") != 0) {
 		std::cerr << "\n" << sEZ_FILE_NAME << " - Warning line " << yylineno << ": Multiple definitions of symbol '" << symbol->sName << "', this may lead to compile errors!\n";
 	}
 	auto h = symbol->sName;
