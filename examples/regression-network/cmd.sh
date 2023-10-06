7a68f3730680b0d125725858d3d40fe1ca8c69e8
zeFresk <8807862+zeFresk@users.noreply.github.com>
2023-10-06 15:42:36 +0200

    Fix network script

--- examples/regression-network/cmd.sh
+++ examples/regression-network/cmd.sh
@@ -10,10 +10,6 @@ fi
 i=0
 for port in $(cat ip.txt | cut -d ':'  -f 2); do
 	printf "Launching $1 on port: $port\n"
-	if [ $i == 0 ]; then 
-		./$1 --serverPort $port --seed $((seed + port)) --ipFile ip.txt &
-	else
-		./$1 --serverPort $port --seed $((seed + port +i)) &
-	fi
+	./$1 --serverPort $port --seed $((seed + port + i)) --ipFile ip.txt &
 	i=$((i+1))
 done
