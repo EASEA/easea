2b9e5d7c793e3975e57cd1c15ac85c4e26391249
zeFresk <8807862+zeFresk@users.noreply.github.com>
2023-10-06 15:58:25 +0200

    Fix network script

--- examples/regression-network/cmd.sh
+++ examples/regression-network/cmd.sh
@@ -2,6 +2,12 @@
 # Start multiple servers from one IP file
 # Léo Chéneau on 2022-06-23
 
+if [[ $1 == "" ]]; then
+	printf "Error: you must provide an EASEA executable as first parameter of this program!\n"
+	printf "usage: cmd.sh <EZ_BINARY> [seed]\n"
+	exit 1
+fi
+
 seed=$2
 if [[ $seed == "" ]]; then
 	seed=0
